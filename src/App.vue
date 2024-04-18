ec88b96dd11a8599eb7a2349c82345b5a7f0b550
Chad Crissman <cdcrissman@gmail.com>
2024-04-18 12:20:27 -0400

    create splash page

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,43 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="headline">
+      <p>Cold fingers, sweaty palms?</p>
+      <p><em>Get a Grip</em></p>
+    </div>
+
+    <img src="./assets/logo.png" class="logo" alt="Digit Defenders Finger Gloves" />
+
+    <p class="coming-soon">Coming Soon!</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
+.headline {
+  text-align: left;
+  margin-bottom: 4rem;
+}
+
+.headline p {
+  font-size: clamp(2rem, 5vw, 4rem);
+  font-weight: 700;
+  text-transform: uppercase;
+  line-height: 1;
+  margin: 0;
+}
+.headline em {
+  color: var(--orange);
+  font-size: clamp(1em, 13vw, 2.5em);
+  font-style: normal;
+  letter-spacing: .1em;
+}
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+  max-height: 30rem;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.coming-soon {
+  font-size: clamp(2rem, 5vw, 4rem);
+  font-style: italic;
+  font-weight: 700;
+  margin-top: 1em;
 }
 </style>
